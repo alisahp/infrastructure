8390338cd0af3a5f19f8cd1d75bb0ac80cdb265d
farrukh90 <farrukhsadykov@gmail.com>
2019-05-25 18:53:31 -0500

    added jenkins

---
+++ jenkins/jenkins.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+resource "aws_instance" "jenkins" {
+  instance_type               = "${var.instance_type}"
+  ami                         = "${var.ami}"
+  key_name                    = "${var.key_name}"
+  associate_public_ip_address = "true"
+  security_groups             = ["allow_ssh_and_jenkins"]
+
+  provisioner "file" {
+    source      = "vault.service"
+    destination = "/tmp/vault.service"
+
+    connection {
+      host        = "${self.public_ip}"
+      type        = "ssh"
+      user        = "${var.user}"
+      private_key = "${file(var.ssh_key_location)}"
+    }
+  }
+  provisioner "file" {
+    source      = "vault.hcl"
+    destination = "/tmp/vault.hcl"
+
+    connection {
+      host        = "${self.public_ip}"
+      type        = "ssh"
+      user        = "${var.user}"
+      private_key = "${file(var.ssh_key_location)}"
+    }
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host        = "${self.public_ip}"
+      type        = "ssh"
+      user        = "${var.user}"
+      private_key = "${file(var.ssh_key_location)}"
+    }
+
+    inline = [
+      "sudo yum install java-1.8.0-openjdk-devel curl -y",
+      "curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
+      "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
+      "sudo yum install jenkins -y",
+      "sudo systemctl start jenkins"
+    ]
+  }
+  tags = {
+    Name = "Vault"
+  }
+}
---
+++ jenkins/sec_group.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "allow_ssh_and_jenkins" {
+  name        = "allow_ssh_and_jenkins"
+  description = "Allow SSH and jenkins"
+  vpc_id      = "${var.vpc_id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
