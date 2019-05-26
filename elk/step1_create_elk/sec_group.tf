33d137b1fb1cfc600ccfeb7ca816de6268c1e411
farrukh90 <farrukhsadykov@gmail.com>
2019-05-25 21:50:56 -0500

    sec opened for web

--- elk/step1_create_elk/sec_group.tf
+++ elk/step1_create_elk/sec_group.tf
@@ -16,6 +16,12 @@ resource "aws_security_group" "allow_ssh_and_elk" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 5600
     to_port     = 5601
     protocol    = "tcp"
