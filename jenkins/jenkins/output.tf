d32912189475364374963931d5e2bb952e048581
farrukh90 <farrukhsadykov@gmail.com>
2019-05-25 19:06:58 -0500

    added 8080

--- jenkins/jenkins/output.tf
+++ jenkins/jenkins/output.tf
@@ -1,5 +1,5 @@
 output "domain" {
-  value = " http://${aws_route53_record.jenkins.name}"
+  value = " http://${aws_route53_record.jenkins.name}:8080"
 }
 output "To_Do"{
   value = "Please reset username and password"
