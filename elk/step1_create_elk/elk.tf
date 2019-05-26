15be9828bea718e2199375269d37d1beab7ef44f
farrukh90 <farrukhsadykov@gmail.com>
2019-05-25 19:52:22 -0500

    elk repo added

--- elk/step1_create_elk/elk.tf
+++ elk/step1_create_elk/elk.tf
@@ -9,7 +9,17 @@ resource "aws_instance" "elk" {
   associate_public_ip_address = "true"
   security_groups             = ["allow_ssh_and_elk"]
 
+  provisioner "file" {
+    source      = "elk"
+    destination = "/tmp/elk.repo"
 
+    connection {
+      host        = "${self.public_ip}"
+      type        = "ssh"
+      user        = "${var.user}"
+      private_key = "${file(var.ssh_key_location)}"
+      }
+    }
   provisioner "remote-exec" {
     connection {
       host        = "${self.public_ip}"
@@ -21,17 +31,10 @@ resource "aws_instance" "elk" {
     inline = [
       "wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u73-b02/jdk-8u73-linux-x64.rpm"",
       "sudo yum -y localinstall jdk-8u73-linux-x64.rpm",
-      "sudo rpm --import http://packages.elastic.co/GPG-KEY-elasticsearch"
-      "echo '[elasticsearch-2.x]
-name=Elasticsearch repository for 2.x packages
-baseurl=http://packages.elastic.co/elasticsearch/2.x/centos
-gpgcheck=1
-gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
-enabled=1
-' | sudo tee /etc/yum.repos.d/elasticsearch.repo",
-    "sudo yum -y install elasticsearch",
-    "sudo systemctl start elasticsearch"
-
+      "sudo rpm --import http://packages.elastic.co/GPG-KEY-elasticsearch",
+      "sudo yum -y install elasticsearch",
+      "sudo systemctl start elasticsearch"
+      "sudo mv /tmp/elk.repo /etc/yum.repos.d/elk.repo"
     ]
   }
   tags = {
