83743af7023fc7c5417d89a09ca553c39bf95961
farrukh90 <farrukhsadykov@gmail.com>
2019-05-25 19:04:00 -0500

    jenkins

--- jenkins/jenkins/route53.tf
+++ jenkins/jenkins/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "jenkins" {
   zone_id = "${var.zone_id}"
-  name    = "vault.${var.domain}"   
+  name    = "jenkins.${var.domain}"   
   type    = "A"
   ttl     = "60"
   records = ["${aws_instance.jenkins.public_ip}"]
