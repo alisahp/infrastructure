8418853a088387ca00b6a36fd1d8ba7922d0f559
farrukh90 <farrukhsadykov@gmail.com>
2020-01-23 04:37:21 -0600

    removed step0

--- jenkins/step0_create_key/README.md
+++
@@ -1,2 +0,0 @@
-* run terraform apply and provide region for key to upload
-* This script sends your ssh key to region you specify
--- jenkins/step0_create_key/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-   backend "s3" {
-    bucket = "acirrustech-iaac"
-    region = "us-east-1" 
-    key    = "key/infra"
-  }
-}
