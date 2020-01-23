27d6949521f26862542076916d703bbae30d5682
root <root@ip-172-31-19-165.eu-west-2.compute.internal>
2020-01-23 23:59:41 +0000

    removed nginx

--- jenkins/jenkins.tf
+++ jenkins/jenkins.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "jenkins" {
   associate_public_ip_address = "true"
   security_groups             = ["allow_ssh_and_jenkins"]
 
+
   provisioner "remote-exec" {
     connection {
       host        = "${self.public_ip}"
@@ -15,11 +16,26 @@ resource "aws_instance" "jenkins" {
     }
 
     inline = [
-      "sudo yum install java-1.8.0-openjdk-devel curl -y",
-      "curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
-      "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
-      "sudo yum install jenkins -y",
-      "sudo systemctl start jenkins",
+	"sudo yum install java-1.8.0-openjdk-devel curl -y",
+        "curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
+        "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
+        "sudo yum install jenkins -y",
+        "sudo systemctl start jenkins",
+        "sudo curl -fsSL https://get.docker.com/ | sh",
+	"sudo systemctl enable docker", 
+	"sudo systemctl start docker",
+	"echo usermod",
+        "sudo usermod -aG docker  -s /bin/bash jenkins", 
+	"sudo cp -r /etc/skel/.*	/var/lib/jenkins",
+        "sudo usermod -aG docker jenkins", 
+	"ssh-keygen -b 2048 -t rsa -f /tmp/id_rsa  -q -N ''",
+	"sudo mkdir /var/lib/jenkins/.ssh",
+	"sudo chmod 600 /var/lib/jenkins/.ssh",
+	"sudo cp /tmp/id_rsa*	/var/lib/jenkins/.ssh",
+	"sudo cat /var/lib/jenkins/.ssh/id_rsa.pub",
+	"sudo yum install git -y",
+	
+	
     ]
   }
 
--- jenkins/sec_group.tf
+++ jenkins/sec_group.tf
@@ -9,6 +9,20 @@ resource "aws_security_group" "allow_ssh_and_jenkins" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
 
   ingress {
     from_port   = 8080
