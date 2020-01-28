4300a2ec5c38fd22932b4d8224b72425ca4480b0
root <root@ip-172-31-25-164.eu-west-2.compute.internal>
2020-01-28 10:54:28 +0000

    added role

--- jenkins/jenkins.tf
+++ jenkins/jenkins.tf
@@ -5,12 +5,13 @@ resource "aws_instance" "jenkins" {
   key_name                    = "${var.key_name}"
   associate_public_ip_address = "true"
   security_groups             = ["allow_ssh_and_jenkins"]
+  iam_instance_profile = "${aws_iam_instance_profile.jenkins_profile.name}"
 
 
 
   provisioner "file" {
    connection {
-      host        = "jenkins.${var.domain}"
+      host        = "${self.public_ip}"
       type        = "ssh"
       user        = "${var.user}"
       private_key = "${file(var.ssh_key_location)}"
