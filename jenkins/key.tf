c441e4305cd44ebff1e89d0c787fbc9b90f9cbf5
root <root@ip-172-31-25-164.eu-west-2.compute.internal>
2020-01-29 04:45:57 +0000

    removed

--- jenkins/key.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "jenkins" {
-  key_name   = "${var.key_name}"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
-}
--- jenkins/route53.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "jenkins" {
-  zone_id = "${var.zone_id}"
-  name    = "jenkins.${var.domain}"
-  type    = "A"
-  ttl     = "60"
-  records = ["${aws_instance.jenkins.public_ip}"]
-}
