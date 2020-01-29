888e71b5ad2fec920b641df941b2b2a4c8832c1e
root <root@ip-172-31-25-164.eu-west-2.compute.internal>
2020-01-29 17:10:42 +0000

    remov

--- jenkins/environment_instances/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region = "${var.region}"
-  version = "~> 2.46"
-}
-terraform {
-  required_version = "0.11.14"
-}
--- jenkins/environment_instances/variable.tf
+++
@@ -1,8 +0,0 @@
-variable "instance_type" {}
-variable "key_name" {}
-variable "vpc_id" {}
-variable "user" {}
-variable "ssh_key_location" {}
-variable "zone_id" {}
-variable "domain" {}
-variable "region" {}
