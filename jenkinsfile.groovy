8aa38fe9be0b02dfb53b7519d986d4988bf4efbc
Ali <alisevhp@gmail.com>
2020-02-05 18:48:19 -0600

    added  jenkins file

---
+++ jenkinsfile.groovy
@@ -0,0 +1,20 @@
+node {
+    stage("Stage1")
+        echo "Hello World"
+    }
+}
+    node {
+    stage("Stage2")
+        echo "Hello World"
+    }
+}
+    node {
+    stage("Stage3")
+        echo "Hello World"
+    }
+}
+    node {
+    stage("Stage4")
+        echo "Hello World"
+    }
+}
