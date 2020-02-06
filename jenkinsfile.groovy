80b53a2519ca38a8999c3ee60d50af11b1e288a9
Ali <alisevhp@gmail.com>
2020-02-05 19:30:25 -0600

    as

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,17 +1,33 @@
 node {
     properties([pipelineTriggers([cron('* * * * *')])])
-    stage("Stage1"){
+    stage("Pull Repo"){
+        git 'https://github.com/farrukh90/packer.git'    
+    }
+    stage("Deploy to Dev"){
         echo "Hello World"
     }
-    stage("Stage2"){
+    stage("Wait"){
+        //input 'Deploy to QA? '
+        //sleep 3
+    }
+    stage("Deploy to QA"){
         echo "Hello World"
     }
-    stage("Stage3"){
-        echo "Hello World"
-    }
+    stage("Script"){
+		sh label: '', script: 
+		'''#!/bin/bash
+			if [ ! -f /tmp/foo.txt ]; 
+			then
+				echo "File not found!"
+				echo "Creating a file"
+				touch "/tmp/foo.txt"
+			fi
+		'''
+	}
+
     stage("Send Notification to slack"){
         echo "Hello World"
         slackSend channel: 'nagios_alerts', message: 'Completed'
-        mail bcc: 'alisevhp@gmail.com', body: 'message', cc: 'farrukh@gmail.com', from: '', replyTo: '', subject: 'Completed', to: 'farrukh@gmail.com'
+        mail bcc: 'farrukh@gmail.com', body: 'message', cc: 'farrukh@gmail.com', from: '', replyTo: '', subject: 'Completed', to: 'farrukh@gmail.com'
     }
 }
