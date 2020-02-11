babe1f0534916cbc601e86f5de531d3e97d0bd19
alisahp <alisevhp@gmail.com>
2020-02-11 01:35:49 +0000

    added new changes

--- jenkins/module/jenkins_password.tf
+++ jenkins/module/jenkins_password.tf
@@ -3,7 +3,28 @@ resource "null_resource" "jenkins_passwd" {
     always_run = "${timestamp()}"
   }
   depends_on = ["aws_route53_record.jenkins_master"]
+ provisioner "file" {
+    connection {
+      host        = "jenkins_master.${var.domain}"
+      type        = "ssh"
+      user        = "${var.user}"
+      private_key = "${file(var.ssh_key_location)}"
+    }
 
+   source = "/root/.ssh"
+   destination = "/tmp/"
+ }
+ provisioner "file" {
+    connection {
+      host        = "jenkins_master.${var.domain}"
+      type        = "ssh"
+      user        = "${var.user}"
+      private_key = "${file(var.ssh_key_location)}"
+    }
+   source = "./module/config"
+   destination = "/tmp/config"
+
+ }
  provisioner "remote-exec" {
     connection {
       host        = "jenkins_master.${var.domain}"
@@ -14,8 +35,12 @@ resource "null_resource" "jenkins_passwd" {
 
     inline = [
         "echo -e $(tput setaf 1 )'Jenkins Passwd is: '$(tput sgr0) $(tput setaf 2)`sudo cat /var/lib/jenkins/secrets/initialAdminPassword`$(tput sgr0)",
+        "sudo cp -r /tmp/.ssh/  /var/lib/jenkins",
+        "sudo cp -r /tmp/config  /var/lib/jenkins/.ssh/",
+        "sudo chmod 600 /var/lib/jenkins/.ssh/id_rsa",
+        "sudo chown -R jenkins:jenkins /var/lib/jenkins/.ssh",    
+        
+
     ]
   }
 }
-
-
